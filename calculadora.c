6f50a202322ffe9efdb42a4a8368187846bd4e2a
miguel <mellogomes.miguel88@gmail.com>
2026-06-10 19:41:23 -0300

    Add files via upload

---
+++ calculadora.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "function_multiplicar.c"
+#include "function_dividir.c"
+
+int main(int argc, char  *argv[]){
+
+    if(argc != 4){
+
+        printf("digite o nome do programa seguido de 3 argumentos (ex: calculadora soma 5 2)");
+
+        printf("\nsendo SOMA a operacao, 2 e 3 os valores, esses valores podem ser quaisquer inteiros");
+
+        printf("\n\nAs operacoes disponiveis sao soma, subtracao, multiplicacao, e divisao");
+      
+        return 0;
+    }
+
+    int num1 = atoi(argv[2]);
+
+    int num2 = atoi(argv[3]);
+
+    if(strcmp(argv[1], "soma") == 0){
+
+        printf("O resultado e: %d", num1+num2);
+    }
+
+    if(strcmp(argv[1], "subtracao") == 0){
+
+        printf("O resultado e: %d", num1-num2);
+    }
+
+    if(strcmp(argv[1], "divisao") == 0){
+
+        printf("O resultado e: %d", dividir(num1, num2));
+    }
+
+    if(strcmp(argv[1], "multiplicacao") == 0){
+
+        printf("O resultado e: %d", multiplicar(num1, num2));
+    }
+
+    if(strcmp(argv[1], "soma") == 1 && strcmp(argv[1], "multiplicacao") == 1 && strcmp(argv[1], "subtracao") == 1 && strcmp(argv[1], "divisao") == 1){
+    
+        printf("Operacao invalida");
+    }
+        
+    printf("\nFeito por Miguel");
+    
+    return 0;
+}
---
+++ test_calculadora.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <assert.h>
+#include "function_multiplicar.c"
+#include "function_dividir.c"
+
+int main(){
+
+    assert(multiplicar(2, 0) == 2*0);
+
+    assert(multiplicar(3, 6) == 3*6);
+
+    assert(multiplicar(42, 64) == 42*64);
+
+    assert(dividir(14,2) == 7);
+
+    assert(dividir(99,3) == 33);
+
+    assert(dividir(144,12) == 12);
+
+    printf("Teste executado com sucesso");
+    
+    return 0;
+}
